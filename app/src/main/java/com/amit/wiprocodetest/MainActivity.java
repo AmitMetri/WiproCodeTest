5b94b284ec9915d804b636c22955c3adabc49e97
AmitMetri <amit.e18@gmail.com>
2018-05-20 20:21:07 +0530

    1. Added RecyclerView Refresh functionality

--- app/src/main/java/com/amit/wiprocodetest/MainActivity.java
+++ app/src/main/java/com/amit/wiprocodetest/MainActivity.java
@@ -1,13 +1,17 @@
 package com.amit.wiprocodetest;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.amit.wiprocodetest.Adapters.MyRecyclerViewAdapter;
 import com.amit.wiprocodetest.Interfaces.ItemInterface;
@@ -24,12 +28,16 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.my_recycler_view)
     RecyclerView myRecyclerView;
+    //Not able to bind with butterknife
+    SwipeRefreshLayout mSwipeRefreshLayout ;
+    List<Row> row;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //Set the LinearLayoutManger
         myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //Initialize SwipeRefreshLayout and setOnRefreshListener
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(MainActivity.this);
+
 
         /*Using Retrofit to fetch the JSON data
         * Build the retrofit object*/
@@ -63,18 +75,21 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("MAIN Success", response.toString());
                 Items items = response.body();
                 getSupportActionBar().setTitle(items.getTitle());
-                List<Row> row = items.getRows();
+                row = items.getRows();
                 myRecyclerView.setAdapter(new MyRecyclerViewAdapter(getApplicationContext(), row));
             }
+
             @Override
             public void onFailure(Call<Items> call, Throwable t) {
                 Log.e("MAIN Error", t.toString());
             }
         });
 
+
     }
 
-    @Override
+
+  /*  @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -94,5 +109,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }*/
+
+
+
+    /*Implement onRefresh from SwipeRefreshLayout.OnRefreshListener
+     * to Refresh the RecyclerView */
+    @Override
+    public void onRefresh() {
+
+        new MyRecyclerViewAdapter(getApplicationContext(),row).notifyDataSetChanged();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+                Log.e("Main", "Refreshed");
+            }
+        }, 3000);
+        Log.e("Main", "Refreshing");
     }
 }
